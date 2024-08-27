9b68439e707dac28c81cb65a2df60cde4f433c2a
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-26 23:49:52 -0400

    Agregando React reveal

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./style/main.css";
+import { Slide, Fade} from "react-awesome-reveal";
 
 export default function Main() {
     return (
-        <main className="w-screen h-screen bg-center bg-cover">
-
+        <main className="w-screen h-screen bg-center bg-cover text-white flex justify-center items-center">
+            <Fade duration={2000}>
+                <h1 className="text-5xl font-bold">Bienvenido</h1>
+            </Fade>
         </main>
     )
 }
