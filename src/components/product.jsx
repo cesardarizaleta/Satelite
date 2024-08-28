a4fe2ad82c321012c30e27aeec6d8603259c1f76
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-28 18:25:51 -0400

    arreglando foto y hover product

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './product.css';
 
 export default function Product({name, price, type, image}) {
+
     return (
-        <article className="text-black h-56 overflow-hidden flex rounded-lg shadow-md">
-            <img className="w-1/2" src={image} alt={name} />
+        <article className="text-black h-56 overflow-hidden flex rounded-lg shadow-md
+        transition-all duration-500">
+            <img className="w-1/2 transition-all duration-500" src={image} alt={name} />
             <div className="flex flex-col justify-center h-full w-full p-4">
                 <h3><span>Nombre:</span> {name}</h3>
-                <p><span>Precio:</span> {price}</p>
+                <p><span>Precio:</span> {price} $</p>
                 <p><span>Tipo:</span> {type}</p>
             </div>
         </article>
