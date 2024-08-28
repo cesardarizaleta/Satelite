e653981c77cfa96b259158183891cdec31360161
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-28 15:32:31 -0400

    Agregando navbar y tipos

--- src/pages/content.jsx
+++ src/pages/content.jsx
@@ -3,8 +3,19 @@ import './style/content.css';
 
 export default function Content() {
     return  (
-        <div id="content" className="h-screen w-screen flex flex-col items-center justify-center">
+        <div id="content" className="h-screen w-screen flex flex-col items-center justify-center gap-4">
             <h1 className="text-4xl font-bold">Lista Licores</h1>
+            <nav className="w-4/5 overflow-auto justify-center flex">
+                <ul className="flex items-center gap-4">
+                    <li className="bg-black text-white p-2 px-8 rounded-full">Todos</li>
+                    <li className="bg-black text-white p-2 px-8 rounded-full">Ron</li>
+                    <li className="bg-black text-white p-2 px-8 rounded-full">Vodka</li>
+                    <li className="bg-black text-white p-2 px-8 rounded-full">Whisky</li>
+                    <li className="bg-black text-white p-2 px-8 rounded-full">Tequila</li>
+                    <li className="bg-black text-white p-2 px-8 rounded-full">Cerveza</li>
+                    <li className="bg-black text-white p-2 px-8 rounded-full">Vino</li>
+                </ul>
+            </nav>
             <section>
 
             </section>
