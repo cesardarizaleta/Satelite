514615385500fef84bb867568705ca92783bdd1d
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-30 23:35:36 -0400

    Escala Botones > Listo Satelite Momentaneamente

--- src/pages/content.jsx
+++ src/pages/content.jsx
@@ -22,14 +22,14 @@ export default function Content() {
         <div id="content" className="h-screen w-screen flex flex-col items-center justify-center gap-4">
             <h1 className="text-4xl font-bold">Lista Licores </h1>
             <nav className="w-4/5 overflow-auto justify-center flex">
-                <ul className="flex items-center gap-4">
-                    <li onClick={type} className="bg-black transition-all cursor-pointer text-white p-2 px-8 rounded-full">Todos</li>
-                    <li onClick={type} className="bg-black transition-all cursor-pointer text-white p-2 px-8 rounded-full">Ron</li>
-                    <li onClick={type} className="bg-black transition-all cursor-pointer text-white p-2 px-8 rounded-full">Vodka</li>
-                    <li onClick={type} className="bg-black transition-all cursor-pointer text-white p-2 px-8 rounded-full">Whiskey</li>
-                    <li onClick={type} className="bg-black transition-all cursor-pointer text-white p-2 px-8 rounded-full">Tequila</li>
-                    <li onClick={type} className="bg-black transition-all cursor-pointer text-white p-2 px-8 rounded-full">Ginebra</li>
-                    <li onClick={type} className="bg-black transition-all cursor-pointer text-white p-2 px-8 rounded-full">Brandy</li>
+                <ul className="flex items-center overflow-auto gap-4">
+                    <li onClick={type} className="bg-black transition-all active:scale-95 cursor-pointer text-white p-2 px-8 rounded-full">Todos</li>
+                    <li onClick={type} className="bg-black transition-all active:scale-95 cursor-pointer text-white p-2 px-8 rounded-full">Ron</li>
+                    <li onClick={type} className="bg-black transition-all active:scale-95 cursor-pointer text-white p-2 px-8 rounded-full">Vodka</li>
+                    <li onClick={type} className="bg-black transition-all active:scale-95 cursor-pointer text-white p-2 px-8 rounded-full">Whiskey</li>
+                    <li onClick={type} className="bg-black transition-all active:scale-95 cursor-pointer text-white p-2 px-8 rounded-full">Tequila</li>
+                    <li onClick={type} className="bg-black transition-all active:scale-95 cursor-pointer text-white p-2 px-8 rounded-full">Ginebra</li>
+                    <li onClick={type} className="bg-black transition-all active:scale-95 cursor-pointer text-white p-2 px-8 rounded-full">Brandy</li>
                 </ul>
             </nav>
             <section className="w-4/5 gap-4 grid h-1/2 overflow-auto">
